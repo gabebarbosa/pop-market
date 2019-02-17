32bb05c2382e5dca0d8f9d2f5131cf8313dc07c7
gabebarbosa <gabriel.lemosbarbosa@gmail.com>
2019-02-17 07:51:56 -0300

    adicinando o usuário logado ao criar um novo produto

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,6 +27,8 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
 
+    @product.user_id = current_user.id
+
     respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
